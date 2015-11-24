840a45f2972fc302b4bbc963c50d3d01a85b5f2c
Sam Carecho <sam@digiwise.net>
2015-11-24 14:57:49 -0200

    parallax element added

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -71,7 +71,7 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
 
   // Scroll page to top and expand header
   app.scrollPageToTop = function() {
-    app.$.headerPanelMain.scrollToTop(true);
+    //app.$.headerPanelMain.scrollToTop(true);
   };
 
   app.closeDrawer = function() {
